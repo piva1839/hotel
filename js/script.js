c21cf11a5c2fe9d146e39fa16285d03941676026
Oleksii Oboianskyi <piva1839@gmail.com>
2018-10-25 00:12:52 +0300

    hotel
    hotel

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('.item-card__slider, .autoplay').slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
+});
